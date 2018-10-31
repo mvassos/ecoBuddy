828d53a42f0f9d15414b561e28afdfd210156a21
Alexis Alamilla <alexisalamilla7@gmail.com>
2018-10-31 14:12:12 -0700

    Alexis first commit, pushing activities 2,3,4.

--- app/src/main/java/ecobuddy/cmps121/ecobuddy/MainActivity.java
+++ app/src/main/java/ecobuddy/cmps121/ecobuddy/MainActivity.java
@@ -1,13 +1,71 @@
 package ecobuddy.cmps121.ecobuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Button button2;
+    Button button3;
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+
+
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+
+
+            }
+        });
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity4();
+
+
+            }
+        });
     }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+
+    }
+
+    public void openActivity3() {
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+
+    }
+
+    public void openActivity4() {
+        Intent intent = new Intent(this, Activity4.class);
+        startActivity(intent);
+
+    }
+
+
 }
