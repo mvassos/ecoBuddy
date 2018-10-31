244e37698c98c50d94a38d62c2c3eb41741a617f
Alexis Alamilla <alexisalamilla7@gmail.com>
2018-10-31 15:52:42 -0700

    UI integration

--- app/src/main/java/ecobuddy/cmps121/ecobuddy/Activity2.java
+++ app/src/main/java/ecobuddy/cmps121/ecobuddy/Activity2.java
@@ -1,13 +1,37 @@
 package ecobuddy.cmps121.ecobuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
+
+    Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity5();
+
+
+            }
+        });
+
+
+
+    }
+
+    public void openActivity5() {
+        Intent intent = new Intent(this, Activity5.class);
+        startActivity(intent);
+
     }
 }
