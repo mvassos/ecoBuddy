ffe5ccc371751911a5280742e6960faf615628fd
TJD <tdersch@ucsc.edu>
2018-12-03 18:49:08 -0800

    Changed UI for ecoreminders, added back end for matching activity

--- app/src/main/java/ecobuddy/cmps121/ecobuddy/EcoReminders.java
+++ app/src/main/java/ecobuddy/cmps121/ecobuddy/EcoReminders.java
@@ -8,31 +8,16 @@ import android.widget.Button;
 
 public class EcoReminders extends AppCompatActivity {
 
-    Button app_btn, bill_btn, trash_btn, chores_btn;
+    Button bill_btn, send_msg_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ecoreminders_activity);
 
-        app_btn = (Button) findViewById(R.id.appliances_button);
-        app_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                appliances_nav();
-            }
-        });
 
-        trash_btn = (Button) findViewById(R.id.trash_button);
-        trash_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                trash_nav();
-            }
-        });
-
-        chores_btn = (Button) findViewById(R.id.chores_button);
-        chores_btn.setOnClickListener(new View.OnClickListener() {
+        send_msg_btn = (Button) findViewById(R.id.send_message_btn);
+        send_msg_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 appliances_nav();
